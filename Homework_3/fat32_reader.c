9a57438314ed39634709f93b6feb4778a45abd0c
mosherosensweig <rosensweigmoshe@gmail.com>
2018-04-12 23:53:47 -0400

    ls and info completed

--- Homework_3/fat32_reader.c
+++ Homework_3/fat32_reader.c
@@ -1,7 +1,7 @@
 /***********************************************************
  * TODO: Fill in this area and delete this line
- * Name of program:
- * Authors:
+ * Name of program:FAT reader
+ * Authors: Moshe Rosensweig, Yehuda Gale
  * Description:
  **********************************************************/
 
@@ -16,6 +16,8 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <ctype.h>
+
 
 
 /* Put any symbolic constants (defines) here */
@@ -46,23 +48,32 @@ typedef struct temp_file{
 	char * meat;
 } temp_file;
 fat_info info;
-typedef struct current_directery{
+typedef struct loaded_directory{
 	temp_file * directory;
 	//if I look at file_offsets[x] it will tell me where the file named file_names[x] starts in the directory above
 	int * file_offsets;//cluster number of the start of the file names in the corrisponding spot in the next array 
-	char ** file_names;
-}current_directery;
-static current_directery curr_dir;
+	char * file_names;
+}loaded_directory;
+static loaded_directory curr_dir;
+void insertion_sort(loaded_directory dir);
+void swap(loaded_directory dir, int i, int j);
 static temp_file * initilize_temp_file();
-
+//this is the length of file names (including null terminators)
+//11 charecters, one null terminator and one .
+static int NAME_SIZE = 13; 
 void initilize(char * file_path);
 int read_int(int num_bytes, int offset, char * source);
 void print_info();
+void free_loaded(loaded_directory to_free);
 void add_cluster(temp_file * file_so_far, int cluster_num);
 void load_curr_directory(int dir_start, int first, int size);
 void get_current_dir_list(char * directory);
+int get_file_from_name(char * dir_to_list);
 temp_file * get_file(char * source, int fat_start);
+loaded_directory load_directory(int dir_start);
 int read_int(int num_bytes, int offset, char * source);
+int get_file_start(int dir_location, loaded_directory dir);
+void clean_name(char * name_to_clean);
 void initilize(char * file_path)
 {
 
@@ -88,48 +99,156 @@ void initilize(char * file_path)
 	info.cluster_size = info.BPB_SecPerClus * info.BPB_BytesPerSec;
 	info.first_cluster = info.BPB_BytesPerSec * (info.BPB_RsvdSecCnt + info.BPB_NumFATS * info.BPB_FATSz32) + 0;
 	//reading the first cluster of root directory and loading as current directory
-	load_curr_directory(read_int(4, 44, file_map), True, 0);  
+	curr_dir = load_directory(read_int(4, 44, file_map));  
 }
 //loads current directory into curr_dir
-void load_curr_directory(int dir_start, int first, int size)
+loaded_directory load_directory(int dir_start)
 {
-	if(!first)
-	{
-		//free stuff
-		// number_of_files = size / 32;
-	}
-	else{
-
-	}
+	loaded_directory to_load;
+	
 	//extra one for null terminator
 	printf("%d\n", dir_start);
-	curr_dir.directory = get_file(file_map, dir_start);
-	int number_of_files = (info.cluster_size/32) * curr_dir.directory->num_clusters;
+	to_load.directory = get_file(file_map, dir_start);
+	// this is how many files can fit in the number of clusters we have
+	//leave one extra space for null termination
+	int number_of_files = (info.cluster_size/32) * to_load.directory->num_clusters + 1;
 	//fprintf(stderr, "%d\n", read_int(1, 0,curr_dir.directory->meat));
-	curr_dir.file_names = calloc(number_of_files, sizeof(char *) + sizeof(char)* 12);
-	curr_dir.file_offsets = calloc(number_of_files, sizeof(int));
+	//not 100% sure why this works
+	to_load.file_names = calloc(number_of_files, sizeof(char) * NAME_SIZE -sizeof(char)+ sizeof(char*));
+	to_load.file_offsets = calloc(number_of_files, sizeof(int));
 	// int have_more = True;
-	for (int file_location = 0; file_location <= curr_dir.directory->pointer; file_location += 32)
+	int name_we_are_up_to = 0;
+	for (int file_location = 0; file_location <= to_load.directory->pointer; file_location += 32)
 	{
+		//make sure the file name list is null terminated i.e. when we read a string name that starts with a null, it's the end
+		to_load.file_names[file_location/32* NAME_SIZE] = '\0';
 		//checking if it is deleted
 		printf("looking at location %d\n", file_location);
 		int first_char = 0;
-		first_char = curr_dir.directory->meat[file_location] & 0xFF;
+		first_char = to_load.directory->meat[file_location] & 0xFF;
 		printf("%x\n", first_char);
 		//ignore deleted files
 		if (first_char == 0xE5){
 				printf("%s\n", "deleted");
 				continue;
 		}
-		int atribute = curr_dir.directory->meat[file_location + 11] & 0xFF;
+		int attribute = to_load.directory->meat[file_location + 11] & 0xFF;
 		//skip long names
-		if (atribute == 0x0F){
-			printf("long name\n");
+		if (attribute == 0x0F || attribute == 0x08){
+			printf("long name or system name\n");
 			continue;
 		}
-		file_location += 32;
+		memcpy(&to_load.file_names[(name_we_are_up_to) * NAME_SIZE], &to_load.directory->meat[file_location] , NAME_SIZE -1);
+		to_load.file_names[name_we_are_up_to* NAME_SIZE + NAME_SIZE - 2] = '\0';
+		clean_name(&to_load.file_names[(name_we_are_up_to) * NAME_SIZE]);
+		to_load.file_offsets[name_we_are_up_to] = file_location;
+		name_we_are_up_to++;
+	}
+	insertion_sort(to_load);
+	return to_load;
+}
+void clean_name(char * name_to_clean)
+{
+	if(name_to_clean[0] == 0) return;
+	printf("about to clean:%s\n",name_to_clean);
+	char extention[5];
+	char final_name[NAME_SIZE]; 
+	for(int j =0; j < NAME_SIZE; j++) final_name[j] = 0;
+	extention[0] = 0;
+	if (name_to_clean[8] != ' '){
+		extention[0] = '.';
+		extention[1] = name_to_clean[8];
+		extention[2] = name_to_clean[9];
+		extention[3] = name_to_clean[10];
+		extention[4] = 0;
+	}
+	int i;
+	for(i = NAME_SIZE - 5; i >= 0; i--){
+		if(name_to_clean[i - 1] != ' '){
+			break;
 		}
 	}
+	printf("i is %d", i);
+	strncpy(final_name, name_to_clean, i);
+	strcat(final_name, extention);
+	strcpy(name_to_clean, final_name);
+	printf("cleaned:%s\n",name_to_clean);
+}
+void ls(char * dir_to_list)
+{
+	int dir_to_ls;
+	dir_to_ls = get_file_from_name(dir_to_list);
+	if(dir_to_ls == -1)
+	{
+		printf("Error: No Directory with that name\n");
+		return;
+	}
+	else if (strchr(dir_to_list, '.') !=0 && strchr(dir_to_list, '.') != dir_to_list){
+		printf("Error: That is a file not a directory\n");
+		return;
+	}
+	//by default assume ls has no argument
+	loaded_directory dir = curr_dir; 
+	//if it has an argument load the sub-directory
+	if(dir_to_ls != -2){
+		int begin_cluster = get_file_start(dir_to_ls, curr_dir);
+		dir = load_directory(begin_cluster);
+	}
+	char buff[NAME_SIZE];
+	for(int name_start = 0; dir.file_names[name_start] != '\0'; name_start += NAME_SIZE){
+		printf("%s\t",&dir.file_names[name_start]);
+	}
+	printf("\n");
+	if(dir_to_ls != -2) free_loaded(dir);
+
+}
+void free_loaded(loaded_directory to_free)
+{
+	free(to_free.file_names);
+	free(to_free.file_offsets);
+	free(to_free.directory->meat);
+	free(to_free.directory);
+}
+//dir_location 	= byte offset where the record is stored
+//dir 			= directory it is stored in
+int get_file_start(int dir_location, loaded_directory dir)
+{
+	int clus_start = 0;
+	//first read high and mask off first 2 bytes because the addresses are only 28 bit
+	clus_start = dir.directory->meat[dir_location + 20] & 0xFF;
+	clus_start <<= 16;
+	//then read low
+	clus_start |= dir.directory->meat[dir_location + 26] & 0xFFFF;
+	printf("%s cluster number %x\n", &dir.file_names[dir_location/32* NAME_SIZE], clus_start);
+	return clus_start;
+}
+int get_file_from_name(char * dir_to_list)
+{
+	printf("file name is 0*%s*0\n", dir_to_list);
+
+	//remove starting space
+	while(dir_to_list[0] == ' '){
+		printf("--%c--\n", dir_to_list[0]);
+		dir_to_list = &dir_to_list[1];
+	}
+	dir_to_list[strlen(dir_to_list) - 1] = 0;
+	printf("file name is 0*%s*0\n", dir_to_list);
+	//create padded name to allow comparisons
+	//test if it's empty
+	if(dir_to_list[0] =='\0') return -2;
+	char dir_name[NAME_SIZE];
+	for(int i = 0; i < NAME_SIZE; i++) dir_name[i] = '\0';
+	strcpy(dir_name, dir_to_list);
+	for(int i = 0; i < NAME_SIZE; i++) dir_name[i] = toupper(dir_name[i]);
+	printf("file name is 0*%s*0\n", dir_name);
+	for(int name_start = 0; curr_dir.file_names[name_start] != '\0'; name_start += NAME_SIZE){
+		printf("name:--%s-- compare --%s--\n", &curr_dir.file_names[name_start], dir_name);
+		if (strncmp(&curr_dir.file_names[name_start], dir_name, NAME_SIZE) == 0){
+			return curr_dir.file_offsets[name_start / NAME_SIZE];
+		}
+	}
+	return -1;
+	
 }
 int read_int(int num_bytes, int offset, char * source)
 {
@@ -172,10 +291,7 @@ void print_info()
 // 	fprintf(stdout, "BPB_FATSz32 is 0x%x, %d\n", temp_var, temp_var);
 
 // }
-void get_current_dir_list(char * directory)
-{
 
-}
 temp_file * get_file(char * source, int fat_start)
 {
 	temp_file * new_file = initilize_temp_file();
@@ -282,7 +398,8 @@ int main(int argc, char *argv[])
 		}
 
 		else if(strncmp(cmd_line,"ls",2)==0) {
-			printf("Going to ls.\n");
+			//printf("file offset is :%d", get_file_from_name(&cmd_line[2]));
+			ls(&cmd_line[2]);
 		}
 
 		else if(strncmp(cmd_line,"read",4)==0) {
@@ -302,5 +419,31 @@ int main(int argc, char *argv[])
 	/* Close the file */
 
 	return 0; /* Success */
+}
+void insertion_sort(loaded_directory dir)
+{
+	int index_of_next_spot = 0;
+	int index_of_min_value = index_of_next_spot;
+	for(int outer = 0; dir.file_names[outer*NAME_SIZE] != '\0'; outer += 1){
+		for(int inner = outer; dir.file_names[inner*NAME_SIZE] != '\0'; inner += 1){
+			if(strncmp(&dir.file_names[inner*NAME_SIZE], &dir.file_names[outer*NAME_SIZE], NAME_SIZE) < 0){
+				swap(dir, inner, outer);
+			}
+		}
+
+	}
+
+}
+void swap(loaded_directory dir, int i, int j)
+{
+	char tmp_name[NAME_SIZE];
+	int tmp_loc;
+	strncpy(tmp_name, &dir.file_names[i * NAME_SIZE], NAME_SIZE);
+	strncpy(&dir.file_names[i * NAME_SIZE], &dir.file_names[j * NAME_SIZE], NAME_SIZE); 
+	strncpy(&dir.file_names[j * NAME_SIZE], tmp_name, NAME_SIZE);
+	tmp_loc = dir.file_offsets[i];
+	dir.file_offsets[i] = dir.file_offsets[j];
+	dir.file_offsets[j] = tmp_loc;
+	
 }
 
